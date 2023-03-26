cc26214c0d61ec7616b34ae91ef36ac6e30aa0fc
Piotr Kot <piterek130@gmail.com>
2023-03-27 01:40:55 +0200

    Rama do sortowań

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -226,7 +226,7 @@ int PartitionLamuto(int L, int R) {
 int chuj = 0;
 void QuickSortR(int L, int R, int iloscLiczb) {
     if (R - L + 1 <= 1) {
-        print_progress((float)chuj++/iloscLiczb);
+        //print_progress((float)chuj++/iloscLiczb);
         //bubbleSort(liczby, R, L);
         return;
     }
@@ -289,6 +289,84 @@ int zapisDoPlikuCzas(int j)
     }
     plik.close();
 }
+
+void heapify(int liczby[], int n, int i) {
+    int largest = i; // węzeł o największej wartości
+    int l = 2*i + 1; // lewe dziecko
+    int r = 2*i + 2; // prawe dziecko
+
+    // Sprawdzamy, czy lewe dziecko jest większe od węzła
+    if (l < n && liczby[l] > liczby[largest])
+        largest = l;
+
+    // Sprawdzamy, czy prawe dziecko jest większe od węzła
+    if (r < n && liczby[r] > liczby[largest])
+        largest = r;
+
+    // Jeśli największy węzeł nie jest korzeniem
+    if (largest != i) {
+        swap(liczby[i], liczby[largest]);
+
+        // Rekurencyjnie wywołujemy heapify dla poddrzewa, które może być zmienione
+        heapify(liczby, n, largest);
+    }
+}
+
+void heapSort(int liczby[], int n) {
+    // Budujemy kopiec (najpierw z ostatniego węzła, który ma dzieci)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(liczby, n, i);
+
+    // Przechodzimy po elementach kopca i zamieniamy największy element z ostatnim elementem
+    for (int i=n-1; i>=0; i--) {
+        swap(liczby[0], liczby[i]);
+
+        // Wywołujemy heapify tylko dla poddrzewa o rozmiarze i
+        heapify(liczby, i, 0);
+    }
+}
+
+
+void countingSort(int arr[], int n) {
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i =0; i<n;i++)
+    {
+        if(min>arr[i])
+        {
+            min=arr[i];
+        }
+        if(max<arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    int size = max-min+1;
+    int tab[size];
+    for (int i = 0; i < size; ++i)
+        tab[i] = 0;
+    int przesuniecie;
+    if (min<0){
+        przesuniecie=min;
+    }else{
+        przesuniecie=0;
+    }
+    for (int i = 0; i < n; ++i)
+        tab[arr[i]-przesuniecie]++;
+
+    for (int i = 1; i < size; ++i)
+        tab[i] += tab[i-1];
+
+    int sortedArr[size];
+
+    for (int i = 0; i < n; ++i)
+        sortedArr[--tab[arr[i]-przesuniecie]] = arr[i];
+
+    for (int i=0; i<n;i++)
+        arr[i]=sortedArr[i];
+}
+
 
 
 int main()
@@ -372,7 +450,54 @@ int main()
             zapisDoPlikuCzas(1);
             zapisDoPlikuCzas(2);
 
-        }
+        }else if (sortowanie == "kopcowanie")
+            {
+                //auto start = high_resolution_clock::now();
+                cout<<"================================="<<endl;
+                int iloscLiczb = odczytLiczb(i)-1;
+                //int n = sizeof(liczby) / sizeof(liczby[0]);
+                auto start = high_resolution_clock::now();
+                heapSort(liczby, iloscLiczb);
+                auto stop = high_resolution_clock ::now();
+                auto duration = duration_cast<microseconds >(stop - start);
+                //printArray(iloscLiczb);
+                zapisDoPlikuPosortowane(iloscLiczb);
+                delete[] liczby;
+                i++;
+                cout<<"================================="<<endl;
+                cout<<"Rozmiar instancji "<<iloscLiczb << endl;
+//            auto stop = high_resolution_clock ::now();
+//            auto duration = duration_cast<microseconds >(stop - start);
+                cout << "Czas wykonania algorytmu: "
+                     << (double)duration.count()/1000000 << " sekund" << endl;
+
+                czasIWielkoscInstancji.push_back(make_pair((double)duration.count()/1000000, iloscLiczb));
+                //printArray();
+                zapisDoPlikuCzas(1);
+                zapisDoPlikuCzas(2);
+        }else if (sortowanie == "zliczanie")
+        {
+            //auto start = high_resolution_clock::now();
+            cout<<"================================="<<endl;
+            int iloscLiczb = odczytLiczb(i)-1;
+            auto start = high_resolution_clock::now();
+            countingSort(liczby, iloscLiczb);
+            auto stop = high_resolution_clock ::now();
+            auto duration = duration_cast<microseconds >(stop - start);
+            zapisDoPlikuPosortowane(iloscLiczb);
+            delete[] liczby;
+            i++;
+            cout<<"================================="<<endl;
+            cout<<"Rozmiar instancji "<<iloscLiczb << endl;
+//            auto stop = high_resolution_clock ::now();
+//            auto duration = duration_cast<microseconds >(stop - start);
+            cout << "Czas wykonania algorytmu: "
+                 << (double)duration.count()/1000000 << " sekund" << endl;
+
+            czasIWielkoscInstancji.push_back(make_pair((double)duration.count()/1000000, iloscLiczb));
+            //printArray();
+            zapisDoPlikuCzas(1);
+            zapisDoPlikuCzas(2);}
     }
 //    ofstream plik("C:\\Users\\piter\\OneDrive\\Pulpit\\github1\\untitled\\wynik.txt");
 //    if (!plik.is_open()) {
